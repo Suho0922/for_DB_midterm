a628232d3dc07b6a508371e41b9a067c3e490da9
Yang Suho <didtngh97@kaist.ac.kr>
2021-05-16 17:58:37 +0900

    6

--- test01.sql
+++ test01.sql
@@ -1,7 +1,17 @@
+WITH avg_inventory AS(
 WITH film_inventoryCount AS(
 SELECT film_id , COUNT(inventory_id) as inventory_cnt
 FROM inventory 
 GROUP BY film_id
 )
 SELECT AVG(inventory_cnt) as inventory_avg
-FROM film_inventoryCount;
+FROM film_inventoryCount
+),
+film_inventoryCount2 AS(
+SELECT film_id , COUNT(inventory_id) as inventory_cnt
+FROM inventory 
+GROUP BY film_id
+)
+SELECT film_id
+FROM film_inventoryCount2, avg_inventory
+WHERE inventory_cnt > avg_inventory.inventory_avg;
