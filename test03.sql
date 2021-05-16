305941a9b4010810347f91a4e5eb32f95818cb57
Yang Suho <didtngh97@kaist.ac.kr>
2021-05-16 18:15:48 +0900

    6

--- test03.sql
+++ test03.sql
@@ -1,3 +1,4 @@
+WITH film_rent_cnt AS(
 WITH film_rental AS(
 SELECT film_id,rental_id
 FROM rental JOIN inventory
@@ -5,4 +6,8 @@ ON rental.inventory_id = inventory.inventory_id
 )
 SELECT film_id, COUNT(rental_id) as rent_cnt
 FROM film_rental
-GROUP BY film_id;
+GROUP BY film_id
+)
+SELECT film_id, rent_cnt 
+FROM film_rent_cnt
+WHERE rent_cnt >= 30;
