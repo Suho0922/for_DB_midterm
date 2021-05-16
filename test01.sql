4c7fd2971d98595d4eb8a31fdad6424693c9c14c
Yang Suho <didtngh97@kaist.ac.kr>
2021-05-16 14:33:31 +0900

    2

--- test01.sql
+++ test01.sql
@@ -1,3 +1,3 @@
 SELECT inventory.inventory_id, film_id, store_id 
 FROM inventory JOIN rental 
-WHERE invetory.inventory_id = rental.inventory_id;
+WHERE inventory.inventory_id = rental.inventory_id;
