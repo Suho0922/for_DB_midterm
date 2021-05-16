844ec68110a354d0d3c77d34868a995ef1079644
Yang Suho <didtngh97@kaist.ac.kr>
2021-05-16 15:21:32 +0900

    3

--- test01.sql
+++ test01.sql
@@ -12,4 +12,4 @@ AND unix_timestamp(rental_date) <= unix_timestamp('2005.08.31')
 )
 SELECT *
 FROM inven_film JOIN rent_date_between
-WHERE inven_flim.inventory_id = rent_date_between.inventory_id;
+WHERE inven_film.inventory_id = rent_date_between.inventory_id;
