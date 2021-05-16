76a7deaf539d0337426e05960581a0183ce9ebd8
Yang Suho <didtngh97@kaist.ac.kr>
2021-05-16 13:28:06 +0900

    6

--- test01.sql
+++ test01.sql
@@ -1,3 +1,4 @@
+WITH f_c_count AS(
 WITH f_c AS(
 SELECT film_category.film_id, category_id, actor_id 
 FROM film_category 
@@ -7,4 +8,7 @@ ON film_category.film_id = film_actor.film_id
 SELECT f_c.actor_id, f_c.category_id, COUNT(film_id)
 FROM f_c
 GROUP BY f_c.actor_id, f_c.category_id
-WHERE COUNT(film_id) > 6;
+)
+SELECT *
+FROM f_c_count
+WHERE COUNT(film_id)>5;
