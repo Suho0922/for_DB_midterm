e983d087539bbef4b1a37521c52dfc4ef5ccd7c8
Yang Suho <didtngh97@kaist.ac.kr>
2021-05-16 16:42:22 +0900

    5

--- test01.sql
+++ test01.sql
@@ -1,29 +1,8 @@
-WITH customer_overdued_count AS(
-WITH customer_overdued AS(
-WITH customer_1 AS(
-SELECT * 
-FROM customer 
-WHERE store_id = 1
-),
-overdued_rental AS(
-SELECT * 
-FROM rental 
-WHERE rental_date + interval 30 day < CURRENT_DATE() AND return_date is NULL
+WITH actor_film_cnt AS(
+SELECT actor_id, COUNT(film_id) as film_cnt
+FROM film_actor
+GROUP BY actor_id
 )
-SELECT customer_1.customer_id, rental_id 
-FROM customer_1, overdued_rental
-WHERE customer_1.customer_id = overdued_rental.customer_id
-)
-SELECT customer_id, COUNT(rental_id) as overdued_count 
-FROM customer_overdued
-GROUP BY customer_id
-),
-customer_1_id AS(
-SELECT *
-FROM customer
-WHERE store_id = 1
-)
-SELECT customer_1_id.customer_id, overdued_count
-FROM customer_1_id LEFT JOIN customer_overdued_count
-ON customer_1_id.customer_id = customer_overdued_count.customer_id
-WHERE customer_1_id.customer_id < 100;
+SELECT actor_id
+FROM actor_film_cnt
+WHERE film_cnt > 30 ;
