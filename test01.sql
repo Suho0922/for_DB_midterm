758e7593da9a5501cdef0b9f0046ddf49cb0d0ce
Yang Suho <didtngh97@kaist.ac.kr>
2021-05-16 13:20:12 +0900

    test01

--- test01.sql
+++ test01.sql
@@ -1,4 +1,9 @@
+WITH f_c AS(
 SELECT film_category.film_id, category_id, actor_id 
 FROM film_category 
 JOIN film_actor 
-ON film_category.film_id = film_actor.film_id;
+ON film_category.film_id = film_actor.film_id
+)
+SELECT f_c.actor_id, f_c.category_id, COUNT(film_id)
+FROM f_c
+GROUP BY f_c.actor_id, f_c.category_id;
