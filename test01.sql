ad2b0800b85e0e11af8f98c0cdd70852e0f0255e
Yang Suho <didtngh97@kaist.ac.kr>
2021-05-16 20:37:24 +0900

    8

--- test01.sql
+++ test01.sql
@@ -1,3 +1,4 @@
+WITH cat_store_numRent AS(
 WITH cat_store_rental AS(
 WITH inv_cat_store AS(
 SELECT inventory_id, category_id, store_id
@@ -10,5 +11,8 @@ ON rental.inventory_id = inv_cat_store.inventory_id
 )
 SELECT category_id, store_id, COUNT(rental_id) as num_of_rental 
 FROM cat_store_rental
-GROUP BY category_id, store_id;
-
+GROUP BY category_id, store_id
+)
+SELECT *
+FROM cat_store_numRent
+WHERE num_of_rental > 500;
