301acc57c7c04a7f8e42f59323add9b1b1ec940f
Yang Suho <didtngh97@kaist.ac.kr>
2021-05-16 16:59:05 +0900

    5

--- test01.sql
+++ test01.sql
@@ -1,3 +1,4 @@
+WITH film_prolific_actor AS(
 WITH prolific_actor AS(
 WITH actor_film_cnt AS(
 SELECT actor_id, COUNT(film_id) as film_cnt
@@ -11,3 +12,12 @@ WHERE film_cnt > 30
 SELECT DISTINCT(film_id)
 FROM film_actor JOIN prolific_actor
 ON film_actor.actor_id = prolific_actor.actor_id;
+),
+film_betweenID_overRate AS(
+SELECT film_id
+FROM film 
+WHERE film_id >= 10 AND film_id < 20 AND rental_rate > 4
+)
+SELECT film_betweenID_overRate.film_id
+WHERE film_betweenID_overRate JOIN film_prolific_actor
+ON film_betweenID_overRate.film_id = film_prolific_actor.film_id;
