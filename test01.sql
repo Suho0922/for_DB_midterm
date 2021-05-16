b3403499a67d7bfc811021601896f901b56d349c
Yang Suho <didtngh97@kaist.ac.kr>
2021-05-16 14:40:52 +0900

    2

--- test01.sql
+++ test01.sql
@@ -1,3 +1,8 @@
+WITH film_store AS(
 SELECT inventory.inventory_id, film_id, store_id 
 FROM inventory JOIN rental 
-WHERE inventory.inventory_id = rental.inventory_id;
+WHERE inventory.inventory_id = rental.inventory_id
+)
+SELECT store_id, film_id , COUNT(inventory_id) as retent_ct
+FROM film_store
+GROUP BY store_id, film_id;
