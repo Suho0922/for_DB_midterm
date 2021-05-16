88cf1c46c6128977d9986b781d897d75182995d7
Yang Suho <didtngh97@kaist.ac.kr>
2021-05-16 20:31:33 +0900

    8

--- test01.sql
+++ test01.sql
@@ -1,3 +1,8 @@
+WITH inv_cat_store AS(
 SELECT inventory_id, category_id, store_id
 FROM inventory JOIN film_category
-ON inventory.film_id = film_category.film_id;
+ON inventory.film_id = film_category.film_id
+)
+SELECT category_id, store_id, rental_id
+FROM rental JOIN inv_cat_store
+ON rental.inventory_id = inv_cat_store.inventory_id;
