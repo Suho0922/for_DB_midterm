781f53047d3178c70b91db89b876e8a442b80612
Yang Suho <didtngh97@kaist.ac.kr>
2021-05-16 19:34:43 +0900

    7

--- test01.sql
+++ test01.sql
@@ -1,3 +1,4 @@
+WITH country_store AS(
 WITH country_address AS(
 SELECT country_id, address_id
 FROM city JOIN address 
@@ -5,4 +6,8 @@ ON city.city_id = address.city_id
 )
 SELECT country_id, store_id 
 FROM store JOIN country_address
-ON store.address_id = country_address.address_id;
+ON store.address_id = country_address.address_id
+)
+SELECT inventory_id, country_id
+FROM inventory JOIN country_store
+ON inventory.store_id = country_store.store_id;
