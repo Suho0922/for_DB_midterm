53a35292ef3ea8acdc86cdf345f04bfb24b4707a
Yang Suho <didtngh97@kaist.ac.kr>
2021-05-16 15:20:07 +0900

    3

--- test01.sql
+++ test01.sql
@@ -1,3 +1,15 @@
-SELECT * 
+
+WITH inven_film AS (
+SELECT inventory.film_id, title, inventory_id 
 FROM inventory JOIN film
-WHERE inventory.film_id = film.film_id;
+WHERE inventory.film_id = film.film_id
+),
+rent_date_between AS (
+SELECT * 
+FROM rental
+WHERE unix_timestamp(rental_date) >= unix_timestamp('2005.06.01')
+AND unix_timestamp(rental_date) <= unix_timestamp('2005.08.31')
+)
+SELECT *
+FROM inven_film JOIN rent_date_between
+WHERE inven_flim.inventory_id = rent_date_between.inventory_id;
