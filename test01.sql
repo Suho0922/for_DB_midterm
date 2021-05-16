b4eee26a0c264375deb237d10c9e62da77de7c84
Yang Suho <didtngh97@kaist.ac.kr>
2021-05-16 13:34:53 +0900

    7

--- test01.sql
+++ test01.sql
@@ -1,16 +1,14 @@
-
-SELECT *
-FROM 
-(
 WITH f_c AS(
 SELECT film_category.film_id, category_id, actor_id 
 FROM film_category 
 JOIN film_actor 
 ON film_category.film_id = film_actor.film_id
 )
+SELECT *
+FROM 
+(
 SELECT f_c.actor_id, f_c.category_id, COUNT(film_id)
 FROM f_c
 GROUP BY f_c.actor_id, f_c.category_id
 )actor_category
-
 WHERE COUNT(film_id)>5;
