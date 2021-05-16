f217d14c9f5d8b9786a6ef651f2481abc3c3af18
Yang Suho <didtngh97@kaist.ac.kr>
2021-05-16 17:22:35 +0900

    5

--- test01.sql
+++ test01.sql
@@ -1,3 +1,4 @@
+WITH film_satisfied AS(
 WITH film_prolific_actor AS(
 WITH prolific_actor AS(
 WITH actor_film_cnt AS(
@@ -20,4 +21,22 @@ WHERE film_id >= 10 AND film_id < 20 AND rental_rate > 4
 )
 SELECT film_betweenID_overRate.film_id
 FROM film_betweenID_overRate JOIN film_prolific_actor
-ON film_betweenID_overRate.film_id = film_prolific_actor.film_id;
+ON film_betweenID_overRate.film_id = film_prolific_actor.film_id
+),
+customer_film AS (
+SELECT film_id, customer_id 
+FROM rental JOIN inventory 
+ON rental.inventory_id = inventory.inventory_id 
+)
+SELECT DISTINCT(cf_1.customer_id)
+FROM customer_film cf_1 
+WHERE NOT EXISTS(
+	SELECT * 
+	FROM film_satisfied fs
+	WHERE NOT EXISTS(
+		SELECT *
+		FROM customer_film cf_2
+		WHERE cf_2.customer_id = cf_1.customer_id AND
+			cf_2.film_id = fs.film_id
+		)
+);
