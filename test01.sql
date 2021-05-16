bafb6122624c6ddb428f1145b3cd47f6f64c28a9
Yang Suho <didtngh97@kaist.ac.kr>
2021-05-16 16:51:21 +0900

    5

--- test01.sql
+++ test01.sql
@@ -1,3 +1,4 @@
+WITH prolific_actor AS(
 WITH actor_film_cnt AS(
 SELECT actor_id, COUNT(film_id) as film_cnt
 FROM film_actor
@@ -5,4 +6,8 @@ GROUP BY actor_id
 )
 SELECT actor_id
 FROM actor_film_cnt
-WHERE film_cnt > 30 ;
+WHERE film_cnt > 30 
+)
+SELECT DISTINCT(film_id)
+FROM film_actor JOIN prolific_actor
+ON film_actor.actor_id = prolific_actor.actor_id;
