60ad41a9bcacf2e04a6e296c2acfc64ccf679fd2
Yang Suho <didtngh97@kaist.ac.kr>
2021-05-16 19:31:37 +0900

    7

--- test01.sql
+++ test01.sql
@@ -1,3 +1,8 @@
+WITH country_address AS(
 SELECT country_id, address_id
 FROM city JOIN address 
-ON city.city_id = address.city_id;
+ON city.city_id = address.city_id
+)
+SELECT country_id, store_id 
+FROM store JOIN country_address
+ON store.address_id = country_address.address_id;
