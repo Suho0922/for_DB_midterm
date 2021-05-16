0bc8c8ec3876a03142c60ced5551adb1382a195c
Yang Suho <didtngh97@kaist.ac.kr>
2021-05-16 20:34:40 +0900

    8

--- test01.sql
+++ test01.sql
@@ -1,3 +1,4 @@
+WITH cat_store_rental AS(
 WITH inv_cat_store AS(
 SELECT inventory_id, category_id, store_id
 FROM inventory JOIN film_category
@@ -5,4 +6,9 @@ ON inventory.film_id = film_category.film_id
 )
 SELECT category_id, store_id, rental_id
 FROM rental JOIN inv_cat_store
-ON rental.inventory_id = inv_cat_store.inventory_id;
+ON rental.inventory_id = inv_cat_store.inventory_id
+)
+SELECT category_id, store_id, COUNT(rental_id) as num_of_rental 
+FROM cat_store_rental
+GROUP BY category_id, store_id;
+
